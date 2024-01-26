01f4b882ac937f366e87bb51247d99874851db97
CHANDAN KUMAR <121013050+01chandan@users.noreply.github.com>
2024-01-27 00:35:10 +0530

    Add files via upload

--- Basic 8/script.js
+++ Basic 8/script.js
@@ -1,16 +1,47 @@
-//Ques: Can we call function at the top of declaration?
-//Ans: Yes! using Hoisting which is already done by javascript Engine
+// // Slice does not affect original array
+// let arr = [1,5,3,2,6,4,4]
+// console.log(arr);
+// console.log(arr.slice(1, 5));
 
-// function call
-name();
+// // Splice affect original array
+// let myarr = arr
+// console.log(myarr);
+// console.log(myarr.splice(1,2));
+// console.log(myarr);
 
-// function declaration
-function name(){
-    console.log("chandan Naam hai mera bhai");
+// -----------------------------------------------------
+
+// // Try and catch 
+// let arr = [1,5,6,5,2]
+// try {
+//     console.log(arr);
+// } catch (error) {
+//     alert("Please enter correct options")
+// }
+
+// ------------------------------------------
+
+// Getter & Setter
+class user {
+    constructor(email, password){
+        this.email = email,
+        this.password = password
+    }
+
+    get email(){
+        return this._email.toUpperCase()
+    }
+    set email(value){
+        this._email = value
+    }
+    get password(){
+        return `${this._password}chandan`
+    }
+    set password(value){
+        this._password = value
+    }
 }
 
-// Function assignement
-let wood = function call(){
-    console.log("This is a Furniture")
-}
-wood();
+let obj = new user("chandan.ai", 132)
+console.log(obj.email);
+console.log(obj.password);
