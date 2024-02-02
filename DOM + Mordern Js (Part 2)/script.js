81a4eb9f5dda24d32949fc1810ff5eea94930997
CHANDAN KUMAR <121013050+01chandan@users.noreply.github.com>
2024-02-02 23:19:07 +0530

    Add files via upload

---
+++ DOM + Mordern Js (Part 2)/script.js
@@ -0,0 +1,13 @@
+// function change(){
+//     console.log("Hi londe kya haal chal hai bhai ke")
+// }
+// document.addEventListener('click', change());
+// document.removeEventListener('click', chnage())
+
+// //Event Listener not removing in this state because both are different objects
+// document.addEventListener('click', function (){
+//     console.log("Hi londe kya haal chal hai bhai ke")
+// })
+// document.removeEventListener('click', function (){
+//     console.log("Hi londe kya haal chal hai bhai ke")
+// })
